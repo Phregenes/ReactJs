28dbba21bdae24a8206617e41e92a7a0f876f884
Pedro Regenes <phregenes@gmail.com>
2019-11-04 23:15:34 -0300

    1.0

---
+++ src/pages/main/index.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import api from '../../services/api';
+import './styles.css';
+
+export default class Main extends Component {
+  state = {
+    products: [],
+    productInfo: {},
+    page: 1,
+  };
+
+  componentDidMount(){
+    this.loadProducts();
+  }
+
+  loadProducts = async (page = 1) => {
+  
+    const response = await api.get(`/products?page=${page}`); //cria o template string
+
+    const { docs, ...productInfo } = response.data;
+
+    this.setState({ products: docs, productInfo, page });
+  };
+
+  prevPage = () => {
+    const { page, productInfo } = this.state;
+
+    if (page === 1) return;
+
+    const pageNumber = page - 1;
+
+    this.loadProducts(pageNumber);
+  };
+
+  nextPage = () => {
+    const { page, productInfo } = this.state;
+
+    if (page === productInfo.pages) return;
+
+    const pageNumber = page + 1;
+    this.setState({
+      page: pageNumber
+    })
+
+    this.loadProducts(pageNumber);
+  };
+
+  render() {
+    const { products, page } = this.state;
+
+    return (
+      <div className="product-list">
+        {products.map(product => (
+          <article key={product._id}>
+            <strong>{product.title}</strong>
+            <p>{product.description}</p>
+
+            <a href="">Acessar</a>
+          </article>
+        ))}
+        <div className= "actions">
+          <button onClick={() => this.prevPage()}>Anterior</button>
+          <button onClick={this.nextPage}>Proximo</button>
+        </div>
+      </div>
+    );
+    
+  } 
+}
+
+
